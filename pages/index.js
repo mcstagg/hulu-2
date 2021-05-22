a06cdfeb1e9d1c8704a7dfbe09637ab590383113
mcstagg <mcstagg@madisoncollege.edu>
2021-05-22 14:59:41 -0500

    Finished Header Component...
    Added Hero Icons and Header Item component. Also added Hulu logo. Designed everything to be mobile first. Using flex column to achieve result.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
@@ -10,7 +10,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Let's build Hulu 2.0</h1>
+      {/* Header */}
+      <Header />
+
+      {/* NavBar */}
+
+      {/* Results */}
     </div>
   )
 }
